4e409d6718a0bb6472deba20bc988c55ffb952e4
Евгений Антонюк <evgenijantonyuk@gmail.com>
2024-01-30 12:36:17 +0700

    first lesson

---
+++ index.js
@@ -0,0 +1,17 @@
+// Заголовок
+let lessonHeader = 'Домашняя работа по итогам урока 1';
+console.log(lessonHeader)
+// Данные
+const MY_NAME = 'Егений';
+const MY_AGE = 46;
+const FAMILY_STATUS = true;
+// Вывод данных
+console.log(`Имя: ${MY_NAME}. Возраст: ${MY_AGE}. Семейное положение: ${FAMILY_STATUS}`);
+
+// Типы переменных для вывода одной строкой получилось сделать только так:
+console.log(`Имя первой переменной 'MY_NAME', Имя второй переменной 'MY_AGE', Имя третьей переменной 'FAMILY_STATUS'.` + ' Тип первой переменной' + ' ' + typeof(MY_NAME) + '.' + ' Тип второй переменной' + ' ' + typeof(MY_AGE) + '.' + ' Тип третьей переменной' + ' ' + typeof(FAMILY_STATUS) +'.');
+// Типы переменных на разных строках
+console.log(`Имя первой переменной 'MY_NAME', Тип переменной:` + ' ' + typeof(MY_NAME));
+console.log(`Имя первой переменной 'MY_AGE', Тип переменной:` + ' ' + typeof(MY_AGE));
+console.log(`Имя первой переменной 'FAMILY_STATUS', Тип переменной:` + ' ' + typeof(FAMILY_STATUS));
+
