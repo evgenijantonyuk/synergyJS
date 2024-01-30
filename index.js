44a7ee5d3ffb1481039b1983d85551091243fed1
Евгений Антонюк <evgenijantonyuk@gmail.com>
2024-01-30 20:09:44 +0700

    code changed (lesson I)

--- index.js
+++ index.js
@@ -1,17 +1,21 @@
+// ============================================ Урок I ====================================================
+
 // Заголовок
 let lessonHeader = 'Домашняя работа по итогам урока 1';
 console.log(lessonHeader)
 // Данные
-const MY_NAME = 'Егений';
-const MY_AGE = 46;
-const FAMILY_STATUS = true;
+let myName = 'Егений';
+let myAge = 46;
+let myFamilyStatus = true;
 // Вывод данных
-console.log(`Имя: ${MY_NAME}. Возраст: ${MY_AGE}. Семейное положение: ${FAMILY_STATUS}`);
-
+console.log(`Имя: ${myName}. Возраст: ${myAge}. Семейное положение: ${myFamilyStatus}`);
 // Типы переменных для вывода одной строкой получилось сделать только так:
-console.log(`Имя первой переменной 'MY_NAME', Имя второй переменной 'MY_AGE', Имя третьей переменной 'FAMILY_STATUS'.` + ' Тип первой переменной' + ' ' + typeof(MY_NAME) + '.' + ' Тип второй переменной' + ' ' + typeof(MY_AGE) + '.' + ' Тип третьей переменной' + ' ' + typeof(FAMILY_STATUS) +'.');
+console.log(`Имя первой переменной 'MY_NAME', Имя второй переменной 'MY_AGE', Имя третьей переменной 'FAMILY_STATUS'.` + ' Тип первой переменной' + ' ' + typeof(myName) + '.' + ' Тип второй переменной' + ' ' + typeof(myAge) + '.' + ' Тип третьей переменной' + ' ' + typeof(myFamilyStatus) +'.');
 // Типы переменных на разных строках
-console.log(`Имя первой переменной 'MY_NAME', Тип переменной:` + ' ' + typeof(MY_NAME));
-console.log(`Имя первой переменной 'MY_AGE', Тип переменной:` + ' ' + typeof(MY_AGE));
-console.log(`Имя первой переменной 'FAMILY_STATUS', Тип переменной:` + ' ' + typeof(FAMILY_STATUS));
+console.log(`Имя первой переменной 'MY_NAME', Тип переменной:` + ' ' + typeof(myName));
+console.log(`Имя первой переменной 'MY_AGE', Тип переменной:` + ' ' + typeof(myAge));
+console.log(`Имя первой переменной 'FAMILY_STATUS', Тип переменной:` + ' ' + typeof(myFamilyStatus));
+
+// ============================================= Урок II ==================================================
+
 
