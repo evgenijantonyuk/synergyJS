fd744ebbeffbcc79144cf1865963f29dab11d8dc
Евгений Антонюк <evgenijantonyuk@gmail.com>
2024-01-31 13:07:01 +0700

    HomeWork 10.2

--- index.js
+++ index.js
@@ -1,186 +1,60 @@
-// ============================================ Урок I ====================================================
 
-// Заголовок
-let lessonHeader = 'Домашняя работа по итогам урока 1';
-console.log(lessonHeader)
-// Данные
-let myName = 'Егений';
-let myAge = 46;
-let myFamilyStatus = true;
-// Вывод данных
-console.log(`Имя: ${myName}. Возраст: ${myAge}. Семейное положение: ${myFamilyStatus}`);
-// Типы переменных для вывода одной строкой получилось сделать только так:
-console.log(`Имя первой переменной 'MY_NAME', Имя второй переменной 'MY_AGE', Имя третьей переменной 'FAMILY_STATUS'.` + ' Тип первой переменной' + ' ' + typeof(myName) + '.' + ' Тип второй переменной' + ' ' + typeof(myAge) + '.' + ' Тип третьей переменной' + ' ' + typeof(myFamilyStatus) +'.');
-// Типы переменных на разных строках
-console.log(`Имя первой переменной 'MY_NAME', Тип переменной:` + ' ' + typeof(myName));
-console.log(`Имя первой переменной 'MY_AGE', Тип переменной:` + ' ' + typeof(myAge));
-console.log(`Имя первой переменной 'FAMILY_STATUS', Тип переменной:` + ' ' + typeof(myFamilyStatus));
 
-// ============================================= Урок II ==================================================
-let bool = true;
-let number = 654;
-console.log(typeof (bool));
-console.log(typeof (number));
+// Домашняя раблта 10.2
+let homeWork = Number('10.2');
+console.log(homeWork + ' ' + typeof (homeWork));
 
-bool = String (bool);
-number = String (number);
-console.log('Тип данных переменной: ' + typeof (bool));
-console.log('Тип данных переменной: ' + typeof (number));
+let task1 = 'Количество секунд в часе';
+console.log(`Тема: ${task1}`);
 
-let str = '2 + 5';
-console.log('2 + 5');
-// происходит конкатинаия строк(особенность оператора +)
-console.log(3 + '2 + 5');
+let minutesPerHour = 60; // кол-во минут в часе
+let secondsPerMinutes = 60; // кол-во секунд в минуте
+let totalSecondsPerHour = minutesPerHour * secondsPerMinutes; // кол-во секунд
 
-//  ===================== числовые преобразования
-console.log('10' / '5')
-console.log('10' * '5')
-console.log('10' + '5')
-console.log('10' - '5')
+console.log(totalSecondsPerHour);
+console.log(`В часе ${totalSecondsPerHour} сеунд`);
 
-let strNum = '565';
-let boolNum = true;
+// =============================================================================
+let startNumber = 0;
+console.log(startNumber);
 
-console.log('Тип данных strNum: ' + typeof (strNum));
-console.log('Тип данных boolNum: ' + typeof (boolNum));
+startNumber += 7;
+console.log(startNumber);
 
-strNum = Number(strNum);
-boolNum = Number(boolNum);
-// при приведении boolean к числу - получается true=1 / false=0
-console.log('Тип данных strNum: ' + typeof (strNum));
-console.log('Тип данных boolNum: ' + typeof (boolNum));
-console.log(boolNum);
+ startNumber *= 4;
+ console.log(startNumber);
 
-let someFieldInForm = Number('Непереводимая игра слов');
-console.log('Тип данных someFieldInForm: ' + typeof (someFieldInForm));
-console.log(someFieldInForm);
-// специфические значения, которые нужно знать
-let undf = undefined;
-let nl = null;
-let trueVar = true;
-let falseVar = false;
-let emptyString = '';
+startNumber -= 8;
+console.log(startNumber);
 
-undf = Number(undf);
-nl = Number(nl);
-trueVar = Number(true);
-falseVar = Number(false);
-emptyString = Number('');
+startNumber /= 4;
+console.log(startNumber);
 
-console.log(undf, nl, trueVar, falseVar, emptyString);
+startNumber **= 3;
+console.log(startNumber);
 
-// логические преобразования
- undf = undefined;
- nl = null;
-let numVar = 0;
- emptyString = '';
+startNumber %= 5;
+console.log(startNumber);
 
-undf = Boolean(undf);
-nl = Boolean(nl);
-numVar = Boolean(numVar);
-emptyString = Boolean(emptyString);
+// ===========================================================================
+console.log(5 > 4); //true
 
-console.log(undf, nl, numVar, emptyString)
+console.log('ананас' > 'яблоко'); //false
 
-let strBool = 'Hello';
-let strNull = '0';
-let numBool = 564;
+console.log('2' > '12'); //true
 
-strBool = Boolean(strBool);
-strNull = Boolean(strNull);
-numBool = Boolean(numBool);
+console.log(undefined == null); //true
 
-console.log(strBool, strNull, numBool);
+console.log(undefined === null); //false
 
-//  операторы + - / * % ** бинарные
-    let mode = 42 % 9; // вычисление остатка от деления
-    let pow = 7 ** 2; //возведение в степень
-    let pow2 = 25 ** (1/2);
-    
-    console.log(mode, pow, pow2);
-    
-//     унарные
-let neg = -5;
-let sumNew = neg + 7;
-let pos = +6;
-console.log(sumNew, pos);
-// оператор + позволяет получить из строки где записано число само это число
-let strToNum = + '5';
-console.log(strToNum);
-console.log(strToNum + ' ' + typeof (strToNum));
+console.log(null == '0'); //false
 
-// математическое сложение строк
-let strFive ='5';
-let strSeven = '7';
-console.log(strFive + strSeven);
-console.log(+strFive + +strSeven);  // получаем число из строки при помощи +
+console.log(null === +'0'); //false
 
-let numF = 9;
-let strE = '10';
-console.log(numF + strE);  // конкатинация - 910
-console.log(numF + +strE);       // получаем число из строки при помощи +
 
-// присваивание
-let a = 6;
-let b = 9;
-let c = 3 - (a = b + 1);
-console.log(a, c);
 
-// сокращенная запись
-let x = 18;
-x += 2 // x = x + 2
-x /= 2 // x = x / 2
-console.log(x)
 
-x = 2;
-x *= 6 + 3; // x = x * (6 + 3)
-console.log(x)
 
-// инкремент(+1) / дикремент(-1)
-// префиксная форма
-let count = 2;
-console.log(++count);
-console.log(--count);
-// постфиксная форма
-count = 3;
-console.log(count++);
-console.log(count++);
 
-// операторы сравнения
-let g = 5;
-let h = 6;
-console.log(g > h);
-console.log(g < h);
-console.log(g >= h);
-console.log(g <= h);
-console.log(g === h);
-console.log(g !== h);
-// сравниваются коды символов
-console.log('no' > 'yes');
-console.log('yes' > 'no');
 
-// сравнение разных типов
-console.log('5' > 3); //true
-console.log('03' > 1); //true
-console.log(false == 0); //true
-console.log(false == 1); //false
-console.log(false != 0); //false
-console.log(true == 1); //true
-console.log(true == 0); //false
-console.log(false == ''); //true
-// Строгое сравнение сравнивает !типы! операндов
-console.log(false === ''); //false
-console.log(false === 0); //false
-console.log(false !== 1); //true
-
-// только! при нестрогом сравнении null и undefined всегда равны друг другу - true
-//  при строгом нет!
-
-console.log(null > 0); //false
-console.log(null == 0); //false
-console.log(null >= 0); //true
-// undefined  ни с чем сравнивать нельзя!
-console.log(undefined > 0); //false
-console.log(undefined == 0); //false
-console.log(undefined >= 0); //false
 
