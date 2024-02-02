b5cc603415e204eda2bbadef94cde40b8cf5d2ea
Евгений Антонюк <evgenijantonyuk@gmail.com>
2024-02-02 18:49:11 +0700

    lesson 10.4

--- index.js
+++ index.js
@@ -1,111 +1,88 @@
+// ======================================== Циклы WHILE и FOR
+// ================= while
+let f = 0;
+while (f <= 8) {
+    console.log(f)
+    // f++
+    f = f + 1
+}
 
-// Доманее задание 10.3
-let homeWork = 'Доманее задание 10.3';
-console.log(homeWork)
-//  ============================ конструкция if ===========================
-// let hourPart = 33
+let i = 1;
+while (i <= 5) {
+    console.log(i)
+    // i++
+    i = i + 1
+}
+// ============= кол-во букв "b" в слове Bobby
+let str1 = 'Arrow';
+let rLetter = 0;
+i = 0;
+while (i < str1.length) {
+    if (str1[i] === 'r') {
+        rLetter = rLetter + 1
+    }
+    i = i + 1
+}
+console.log(`В слове ${str1} букв "r" ${rLetter}`)
 //
-// if (hourPart < 1 && hourPart <= 15) {
-//  console.log('Перая четверть часа')
-// }
-//
-// if (hourPart >= 15 && hourPart <= 30) {
-//  console.log('Вторая четверть часа')
-// }
-//
-// if (hourPart >= 30 && hourPart <= 45) {
-//  console.log('Третья четверть часа')
-// }
-//
-// if (hourPart >= 45 && hourPart <= 60) {
-//  console.log('Четвертая четверть часа')
-// }
-//
-// if (hourPart > 60) {
-//  console.log('Число больше количества минут в часе')
-// }
-
-// ====================== конструкция if else if =================================
-
-let hourPart = 35;
-
-if (hourPart > 0 && hourPart <= 15)
-{
- console.log('Перая четверть часа');
+let str = 'success';
+let sLetter = 0;
+i = 0
+while (i < str.length) {
+    if (str[i] === 's') {
+        sLetter = sLetter + 1
+    }
+    i = i + 1
 }
-else if (hourPart >= 15 && hourPart <= 30)
-{
- console.log('Вторая четверть часа');
+console.log(`В слове ${str} букв "s" ${sLetter}`)
+
+let div = 2;
+let num = 17;
+let flag = 1;
+
+while (div <= num/2) {
+    if (num % div === 0) {
+        console.log(`Число ${num} не является простым`);
+        flag = 0;
+        break;
+    }
+    div = div + 1;
 }
-else if (hourPart >= 30 && hourPart <= 45)
-{
- console.log('Третья четверть часа');
+if (flag === 1) {
+    console.log(`Число ${num} является простым`);
 }
-else if (hourPart >= 45 && hourPart <= 60)
-{
- console.log('Четвертая четверть часа');
-} else {
- console.log('Число больше количества минут в часе')
+// =================цикл do while
+i = 1
+do {       //тело цикла
+    console.log(i)
+    i++
+} while
+    (i <= 5);    //условие
+
+// ==================== цикл FOR
+for (i = 1; i <= 7; i++) {
+    console.log(i)
 }
 
-// ========================== SWITCH ====================================
-let number = 7, seasonMonth;
-switch (number) {
- case 1:
-  seasonMonth = 'Зима, Январь.';
-  break;
- case 2:
-  seasonMonth = 'Зима, Февраль.';
-  break;
- case 3:
-  seasonMonth = 'Весна, Март.';
-  break;
- case 4:
-  seasonMonth = 'Весна, Апрель.';
-  break;
- case 5:
-  seasonMonth = 'Весна, Май.';
-  break;
- case 6:
-  seasonMonth = 'Лето, Июнь.';
-  break;
- case 7:
-  seasonMonth = 'Лето, Июль.';
-  break
- case 8:
-  seasonMonth = 'Лето, Август.';
-  break;
- case 9:
-  seasonMonth = 'Осень, Сентябрь.';
-  break;
- case 10:
-  seasonMonth = 'Осень, Октябрь.';
-  break;
- case 11:
-  seasonMonth = 'Осень, Ноябрь.';
-  break;
- case 12:
-  seasonMonth = 'Зима, Декабрь.';
-  break;
- default:
-  seasonMonth = 'Такого месяца не существует...';
+for (let k = 1; k <= 3; k = k + 1) {
+    console.log(k)
 }
-console.log(seasonMonth);
+// аерестановка букв в обратном порядке
+str = 'Alex';
+let newStr = '';
+let letter = '';
 
-// ================================
-let digit = 3;
-
-if (digit % 2 === 0) {
- digit = digit ** 2;
-//  сокращенная запись: digit **= 2
-} else {
- digit = digit ** 3;
- //  сокращенная запись: digit **= 3
+for (i = 0; i < str.length; i++) {
+    letter = str[str.length - i - 1]
+    newStr = newStr + letter;
 }
+console.log(newStr);
 
-console.log(digit)
-
-
+//   директива continue
+for (let j = 0; j < 10; j++) {
+    if (j % 2 === 0) continue; // если true, то пропустить оставшуюся часть тела цикла
+    console.log(j)
+}
 
 
 
