eb7e4764b6c4fe50e5f1f93cd978acd542a3a558
Евгений Антонюк <evgenijantonyuk@gmail.com>
2024-02-03 12:17:04 +0700

    HW 10.4

--- index.js
+++ index.js
@@ -1,89 +1,47 @@
-// ======================================== Циклы WHILE и FOR
-// ================= while
-let f = 0;
-while (f <= 8) {
-    console.log(f)
-    // f++
-    f = f + 1
+console.log('Домашнее задание к уроку 10.4')
+
+// Задание 1
+    for (let i = 0; i < 100; i++) {
+        if (i % 3 === 0 && i % 5 === 0) {
+            console.log(`Число ${i} кратно 3 и 5`)
+        }
+            else if (i % 5 === 0) {
+                console.log(`Число ${i} кратно 5`)
+        }
+        else if (i % 3 === 0) {
+            console.log(`Число ${i} кратно 3`)
+        }
+        else {
+            console.log(i)
+        }
 }
 
-let i = 1;
-while (i <= 5) {
-    console.log(i)
-    // i++
-    i = i + 1
-}
-// ============= кол-во букв "b" в слове Bobby
-let str1 = 'Arrow';
-let rLetter = 0;
-i = 0;
-while (i < str1.length) {
-    if (str1[i] === 'r') {
-        rLetter = rLetter + 1
+// Задание 2
+for (i = 1; i < 10; ++i) {
+    for (let c = 0; c < 10; ++c) {
+        for (let b = 0; b < 10; ++b) {
+            let cube = (Math.pow(i, 3) + Math.pow(c, 3) + Math.pow(b, 3))
+            let summa = (i * 100 + c * 10 + b)
+            if (cube === summa) {
+                console.log(`Число Армстронга ${cube}`)
+            }
+        }
     }
-    i = i + 1
 }
-console.log(`В слове ${str1} букв "r" ${rLetter}`)
-//
-let str = 'success';
-let sLetter = 0;
-i = 0
-while (i < str.length) {
-    if (str[i] === 's') {
-        sLetter = sLetter + 1
+// Задание 3
+let fistNumber = 2178
+let secondNumber = 345
+let gcd
+
+while (fistNumber !== secondNumber) {
+    if (fistNumber > secondNumber) {
+        fistNumber = fistNumber - secondNumber
     }
-    i = i + 1
-}
-console.log(`В слове ${str} букв "s" ${sLetter}`)
-
-let div = 2;
-let num = 17;
-let flag = 1;
-
-while (div <= num/2) {
-    if (num % div === 0) {
-        console.log(`Число ${num} не является простым`);
-        flag = 0;
-        break;
+    else {
+        secondNumber = secondNumber - fistNumber
     }
-    div = div + 1;
 }
-if (flag === 1) {
-    console.log(`Число ${num} является простым`);
-}
-// =================цикл do while
-i = 1
-do {       //тело цикла
-    console.log(i)
-    i++
-} while
-    (i <= 5);    //условие
-
-// ==================== цикл FOR
-for (i = 1; i <= 7; i++) {
-    console.log(i)
-}
-
-for (let k = 1; k <= 3; k = k + 1) {
-    console.log(k)
-}
-// аерестановка букв в обратном порядке
-str = 'Alex';
-let newStr = '';
-let letter = '';
-
-for (i = 0; i < str.length; i++) {
-    letter = str[str.length - i - 1]
-    newStr = newStr + letter;
-}
-console.log(newStr);
-
-//   директива continue
-for (let j = 0; j < 10; j++) {
-    if (j % 2 === 0) continue; // если true, то пропустить оставшуюся часть тела цикла
-    console.log(j)
-}
-
-
+gcd = fistNumber
+console.log(`Наибольший общий делитель ${gcd}`)
 
 
