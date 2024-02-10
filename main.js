ce890919d55033f11539008527772846c11f83cc
Евгений Антонюк <evgenijantonyuk@gmail.com>
2024-02-11 02:54:04 +0700

    add js and css to HW 10.9

---
+++ main.js
@@ -0,0 +1,33 @@
+function creatingPage() {
+    let postWrap = document.createElement('div');
+    let postItem = document.createElement('div')
+    let postItemWrap = document.createElement('div');
+    let postLink = document.createElement('a');
+    let postTitle = document.createElement('h3');
+    let postContent = document.createElement('p')
+    
+    postWrap.classList.add('post-wrap');
+    postItem.classList.add('post-item');
+    postItemWrap.classList.add('post-item-wrap');
+    postLink.classList.add('post-link');
+    postTitle.classList.add('post-title');
+    postContent.classList.add('post-content');
+    
+    postTitle.innerHTML = 'My new div';
+    postContent.innerHTML = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum vel arcu purus. Duis egestas purus et ex interdum lobortis. Quisque nec magna viverra, interdum dolor at, congue augue. Quisque sed interdum augue, a lacinia nulla. Sed risus nibh, vestibulum eu nisi eu, imperdiet tempor quam.';
+    
+    document.body.append(postWrap);
+    postWrap.append(postItem);
+    postItem.append(postItemWrap);
+    postItemWrap.append(postLink);
+    postLink.append(postTitle);
+    postLink.append(postContent);
+}
+creatingPage()
+
+
+
+
+
+
+
--- myScript.js
+++ myScript.js
@@ -1,3 +1,19 @@
-function veryShortFunction() {
-    alert('I am here');
+
+function loadNewScript(src, callback) {
+    let script = document.createElement('script');
+    script.src = src;
+    
+    script.onload = () => callback(null, script);
+    script.onerror = () => callback(new Error(`Script download error! ${src}`));
+    
+    document.head.append(script);
 }
+loadNewScript('myScript.js', function (error, script) {
+    if (error) {
+        alert('Error')
+    } else {
+        // alert('Все хорошо!')
+    }
+});
+
+
